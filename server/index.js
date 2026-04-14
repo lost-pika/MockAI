28de6426f83d4cd7ae1296df281aeab949e1ef46
Srishti Verma <srishtiverma358@gmail.com>
2026-04-14 14:10:43 +0530

    fix: cors, vercel headers, firebase auth

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin: process.env.CLIENT_URL || "https://mock-ai-nu.vercel.app/",
+    origin: process.env.CLIENT_URL || "https://mock-ai-nu.vercel.app",
     credentials: true
 }))
 
